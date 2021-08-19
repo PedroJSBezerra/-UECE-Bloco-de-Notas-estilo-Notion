b31c182a39ae97ffb08ac19ec2e55cf920e2a847
PedroJSBezerra <pjsoares46@gmail.com>
2021-08-19 16:27:24 -0300

    svelte update

---
+++ src/components/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAKoiwLVrDDiCLHTIpPgylwlw_gi8XRZ20",
+  authDomain: "xternapp.firebaseapp.com",
+  projectId: "xternapp",
+  storageBucket: "xternapp.appspot.com",
+  messagingSenderId: "740351658749",
+  appId: "1:740351658749:web:9149120ef83c07feb6cc94"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
