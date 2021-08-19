c0ecf94519b80a04808e3a4f544f25c9c4b92ce7
PedroJSBezerra <pjsoares46@gmail.com>
2021-08-18 23:42:22 -0300

    login component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import styled from 'styled-components'
 import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <Slogin>
-        <div className="login">
-          <button><img src="" alt="Login com Google" /></button>
-        </div>
-      </Slogin>
+      <Login />
     </div>
   );
 }
-
-const Slogin = styled.div`
-  flex: 1;
-  .login{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    .button{
-      border: none;
-      background: none;
-    }
-  }
-`
 
 export default App;
