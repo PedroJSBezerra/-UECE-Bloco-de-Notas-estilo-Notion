0f8dd2ac4f7a073b4b7e1c49db9ca1ae0ad13793
PedroJSBezerra <pjsoares46@gmail.com>
2021-08-18 22:55:55 -0300

    login styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Slogin>
+        <div className="login">
+          <button><img src="" alt="Login com Google" /></button>
+        </div>
+      </Slogin>
     </div>
   );
 }
+
+const Slogin = styled.div`
+  flex: 1;
+  .login{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    .button{
+      border: none;
+      background: none;
+    }
+  }
+`
 
 export default App;
