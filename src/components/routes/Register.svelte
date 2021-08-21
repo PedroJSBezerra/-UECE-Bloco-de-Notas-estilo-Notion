5b582e8219dc34a5a3f8501768b756cdc623e44e
PedroJSBezerra <pjsoares46@gmail.com>
2021-08-21 01:22:11 -0300

    svelte events

--- src/components/routes/Register.svelte
+++ src/components/routes/Register.svelte
@@ -1,23 +1,42 @@
 <script>
-  let title = 'Titulo'
-  let text = 'Digite aqui seu texto'
-  
-  const handleclick = (e) => {
-    e.target.innerHTML =''
+  let notes = [
+		{ id: 1, text: 'apple' },
+		{ id: 2, text: 'banana' },
+		{ id: 3, text: 'carrot' },
+		{ id: 4, text: 'doughnut' },
+		{ id: 5, text: 'egg' },
+  ]
+  let uid = notes.length +1
+
+  const add = () => {
+    const note = {
+      id: uid++,
+      text:''
+    }
+    notes = [...notes, note]    
+  }
+  const remove = (note) => {
+    notes = notes.filter( n => n !== note)
+  }
+  const handlekeydown = (e) => {
+    if(e.key == 'Enter'||e.keyCode == 13){
+    }
   }
 </script>
-
-<div class="register" on:click={handleclick}>
-  <div 
-    contenteditable="true" 
-    bind:innerHTML={title} 
-    spellcheck="true" 
-    class="title"
-  ></div>
-  <div contenteditable="true" 
-    bind:innerHTML={text} 
-    spellcheck="true"
-  ></div>
+<!-- modifiador self dispara o evento apenas se for disparado por ele mesmo -->
+<div class="register" on:click|self={add}>
+  {#each notes as note}
+    <div class="note">
+      <div 
+        class="text"
+        bind:innerHTML={note.text} 
+        contenteditable="true" 
+        placeholder="Clique aqui para editar..." 
+        on:keydown={handlekeydown}
+      ></div>
+      <button on:click={() => remove(note)}>Remove</button>
+    </div>
+  {/each}
 </div>
 
 <style>
@@ -26,14 +45,34 @@
     display: flex;
     flex-direction: column;
     font-size: 1.4rem;
+    min-height: 90vh;
   }
-  .title{
-    font-size: 1.8rem;
-  }
-  .register > *{
+  .note{
+    position: relative;
     padding: 5px;
-    background: #444;
+    background: rgba(255,255,255,.2);
     border-radius: 4px;
-    margin-bottom: 2px;
+    margin-bottom: 4px;
+  }
+  .note:hover button {
+    opacity: .4;
+  }
+  .note button:focus{
+    opacity: 1;
+  }
+  .note button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    opacity: .2;
+    margin: 5px;
+    padding: 5px 10px;
+  }
+  [contenteditable]{
+    outline: 0px solid transparent;
+  }
+  [contenteditable]:empty:before{
+    content: attr(placeholder);
+    color: rgba(255,255,255,.4);
   }
 </style>
