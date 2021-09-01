ad7b149945de2de9969472bc74d46f0062c1f445
PedroJSBezerra <pjsoares46@gmail.com>
2021-09-01 16:40:28 -0300

    document v1

--- src/components/Document.svelte
+++ src/components/Document.svelte
@@ -1,26 +1,28 @@
 <script>
-  import Icon_document from '../assets/icon_document.svelte'
-  import Icon_more from '../assets/icon_more.svelte'
-  import Icon_trash from '../assets/icon_trash.svelte'
-  import Icon_share from '../assets/icon_share.svelte'
-  import Icon_close from '../assets/icon_close.svelte'
-  import { fade } from 'svelte/transition'
+  import Icon_document from "../assets/icon_document.svelte";
+  import Icon_more from "../assets/icon_more.svelte";
+  import Icon_trash from "../assets/icon_trash.svelte";
+  import Icon_share from "../assets/icon_share.svelte";
+  import Icon_close from "../assets/icon_close.svelte";
+  import { fade } from "svelte/transition";
 
-  export let document
+  export let document;
 
-  let open = false
-
-  const options = () => {
-    open = !open
-  }
+  let open = false;
 </script>
 
 <div class="document">
   <div class="item_overlay">
     {#if open}
       <div class="options">
-        <div class="icon"><Icon_trash /></div>
-        <div class="icon"><Icon_share /></div>
+        <div class="delete">
+          <div class="icon"><Icon_trash /></div>
+          <span>Apagar</span>
+        </div>
+        <div class="share">
+          <div class="icon"><Icon_share /></div>
+          <span>Compartilhar</span>
+        </div>
       </div>
     {:else}
       <div class="item">
@@ -29,7 +31,7 @@
       </div>
     {/if}
   </div>
-  <div on:click="{options}" class="toggle">
+  <div on:click={() => (open = !open)} class="toggle">
     {#if open}
       <div class="icon"><Icon_close /></div>
     {:else}
@@ -39,30 +41,45 @@
 </div>
 
 <style>
-  .document{
+  .document {
     border-radius: 4px;
     margin: 6px 0;
     display: flex;
-    background: rgba(255,255,255,.2);
+    background: rgba(255, 255, 255, 0.2);
+    overflow: hidden;
   }
-  .icon{
-    margin: .6rem .6rem;
+  .item_overlay {
+    flex: 1;
+    display: flex;
+  }
+  .title {
+    flex: 1;
+  }
+  .options{
+    flex-wrap: wrap;
+  }
+  .toggle {
+    display: flex;
+    align-items: center;
+    background: #555;
+  }
+  .icon {
+    margin: .6rem;
     display: flex;
     align-items: center;
   }
-  .item_overlay{
-    flex: 1;
-    display: flex;
+  span{
+    margin: auto .6rem;
   }
-  .item, .options{
+  .item,
+  .options,
+  .delete,
+  .share{
     flex: 1;
     display: flex;
     align-items: center;
   }
-  .title{
-    flex: 1;
-  }
-  :where(.item, .toggle, .options):hover{
-    background: rgba(255,255,255,.1);
+  :where(.item, .toggle, .delete, .share):hover {
+    background: rgba(255, 255, 255, 0.1);
   }
 </style>
